49006431641b0c2489189daf1ce54dec35d320ad
harshitScript <www.harshitbhawsar777@gmail.com>
2024-08-14 16:19:19 +0000

    [MIDDLEWARE]: notFoundMiddleware corrected

--- src/middlewares/notFound.middleware.ts
+++ src/middlewares/notFound.middleware.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 
 const notFoundMiddleware: express.RequestHandler = (_, res, __) => {
-    throw new Error('error handler')
     return res.status(404).json({ message: 'Not Found' })
 }
 export default notFoundMiddleware;
