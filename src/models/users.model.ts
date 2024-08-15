767b13c36c07ca3faabc750827875e2554ea52f8
harshitScript <www.harshitbhawsar777@gmail.com>
2024-08-15 14:11:14 +0000

    [USES MODEL]: timestamps added

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -7,6 +7,8 @@ const userSchema = new Schema<UserType>({
     phone: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+}, {
+    timestamps: true
 })
 
 export const User = model('User', userSchema)
